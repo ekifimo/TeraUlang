f8088cf93d315d24c6f8a33523273de052b8c55e
ekifimo <ekipnugraha@gmail.com>
2021-07-12 12:49:08 +0700

    SLIDER NAVIGATION

---
+++ app/src/main/java/pemda/cirebon/teraulang/Fragment/CenteredTextFragment.java
@@ -0,0 +1,41 @@
+package pemda.cirebon.teraulang.Fragment;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import pemda.cirebon.teraulang.R;
+
+public class CenteredTextFragment extends Fragment {
+
+    private static final String EXTRA_TEXT = "text";
+
+    public CenteredTextFragment() {
+
+    }
+
+    public static CenteredTextFragment createFor(String text) {
+        CenteredTextFragment fragment = new CenteredTextFragment();
+        Bundle args = new Bundle();
+        args.putString(EXTRA_TEXT, text);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_text, container, false);
+    }
+}
